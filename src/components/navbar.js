924486865d5c99898fd9e236edc715f88a448685
Aaron Regalado <regalado2829@gmail.com>
2022-05-14 17:14:15 -0400

    Made the Navbar for the Website
    I made the navbar for the website. Instead of using AirBNB's logo, I
    used an image that I found off of the internet for the letter A (which
    stands for my first name). I also styled the logo to fit into the
    navbar. Finally, I styled the navbar to have a box-shadow.

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <img src=
+                "https://i.pinimg.com/736x/b9/c5/34/b9c5344544ba5f44997f4190dfdf273d.jpg" 
+                className="aLogo"
+            />
+        </nav>
+    )
+}
